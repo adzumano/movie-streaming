afc4ac8ffb55ec5b015672297323f83e07480dfc
adzumano <zkaragoyshin1@gmail.com>
2023-05-13 11:03:31 +0500

    uninstalled unused packages and initialized root component

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
-import { render } from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-render(<div>App</div>, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <div>App</div>
+);
+
