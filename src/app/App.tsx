b11b849c988ea265f475e912da0ebf922410fabd
adzumano <zkaragoyshin1@gmail.com>
2023-05-13 11:57:14 +0500

    test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,6 @@
-import classNames from 'classnames';
 import { Suspense } from 'react';
 
 export const App = () => {
-    const test = '';
     return (
         <div className={'app'}>
             <Suspense fallback={'loading..'}>App</Suspense>
