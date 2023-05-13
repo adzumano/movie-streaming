d32fe902e7905071925afde24659b239e6f1f375
adzumano <zkaragoyshin1@gmail.com>
2023-05-13 14:50:17 +0500

    added storage service

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,15 @@
 import { AppRouter } from 'app/providers/router';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
+import { KVStorage, LocalStorage } from 'shared/lib/storage';
 import { Header } from 'widgets/Header';
 
 export const App = () => {
+    const storage = new KVStorage(new LocalStorage());
+
+    useEffect(() => {
+        storage.set('key', 'value');
+        console.log(storage.get('key'));
+    }, []);
     return (
         <div className={'app'}>
             <Header />
