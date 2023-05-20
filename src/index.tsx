cc5bbea6f7ebda8465b687b4a86e0c8734ddad17
adzumano <zkaragoyshin1@gmail.com>
2023-05-21 00:49:22 +0500

    added internalization

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import { App } from 'app/App';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import 'app/styles/index.scss';
-import React, { StrictMode } from 'react';
+import React, { StrictMode, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import 'shared/config/i18n/i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <StrictMode>
         <ErrorBoundary>
             <BrowserRouter>
-                <App />
+                <Suspense fallback={'loading..'}>
+                    <App />
+                </Suspense>
             </BrowserRouter>
         </ErrorBoundary>
     </StrictMode>,
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames';
 import { FC, memo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import cls from './Header.module.scss';
 
@@ -7,5 +8,6 @@ interface NavbarProps {
     className?: string;
 }
 export const Header: FC<NavbarProps> = memo(({ className }) => {
-    return <header className={cn(cls.Header, className)}>Header</header>;
+    const { t } = useTranslation();
+    return <header className={cn(cls.Header, className)}>{t('test')}</header>;
 });
